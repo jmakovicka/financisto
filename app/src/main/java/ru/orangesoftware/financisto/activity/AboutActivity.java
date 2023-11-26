0f2d53939788069a627781500d8af648fa22ba00
Jindřich Makovička <makovick@gmail.com>
2023-11-26 14:55:47 +0100

    wip2

--- app/src/main/java/ru/orangesoftware/financisto/activity/AboutActivity.java
+++ app/src/main/java/ru/orangesoftware/financisto/activity/AboutActivity.java
@@ -16,6 +16,9 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TabHost;
+
+import com.google.android.material.tabs.TabLayout;
+
 import ru.orangesoftware.financisto.R;
 import ru.orangesoftware.financisto.utils.MyPreferences;
 import ru.orangesoftware.financisto.utils.Utils;
@@ -32,9 +35,17 @@ public class AboutActivity extends AbstractActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState ) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
         setTitle("Financisto ("+getAppVersion(this)+")");
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("New"));
+        tabLayout.addTab(tabLayout.newTab().setText("Privacy"));
+        tabLayout.addTab(tabLayout.newTab().setText("License"));
+        tabLayout.addTab(tabLayout.newTab().setText("About"));
     }
 
     public static String getAppVersion(Context context) {
