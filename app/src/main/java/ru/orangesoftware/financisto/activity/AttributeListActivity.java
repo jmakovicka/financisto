bb7fa77e14bf9dd6484ed4ae92447ff6dff7a658
Jindřich Makovička <makovick@gmail.com>
2023-12-17 18:19:31 +0100

    AttributeListActivity: Recreate the cursor manually

--- app/src/main/java/ru/orangesoftware/financisto/activity/AttributeListActivity.java
+++ app/src/main/java/ru/orangesoftware/financisto/activity/AttributeListActivity.java
@@ -61,7 +61,8 @@ public class AttributeListActivity extends AbstractListActivity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == RESULT_OK) {
-			cursor.requery();
+			createCursor();
+			recreateAdapter();
 		}
 	}
 
@@ -75,8 +76,9 @@ public class AttributeListActivity extends AbstractListActivity {
 				@Override
 				public void onClick(DialogInterface arg0, int arg1) {
 					db.deleteAttribute(id);
-					cursor.requery();
-				}				
+					createCursor();
+					recreateAdapter();
+				}
 			})
 			.setNegativeButton(R.string.cancel, null)
 			.show();		
